d3707092f3479b3a21620d231c8a9adc88876c95
[oksana-furman] <[oksan9a.furman@gmail.com]>
2023-02-11 14:23:35 +0100

    add coments

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,9 @@
+//put data from json file in the variable 
 var allTasks = JSON.parse(tasks);
 
+//create a loop to to display data from var allTasks in cards 
 for (let val of allTasks) {
+    //create variable for color change of priority button
     let color = "";
     if (val.importance <= 1) {
         color = "green";
@@ -10,7 +13,9 @@ for (let val of allTasks) {
         color = "red";
     }
 
+    //card, displayed on the page with data from json file. it has 3 buttons with events: 1st button(class="btnPriority") when clicked changes number and color. 2nd button(class="btn btn-danger delete") when clicked removes the card. 3rd button(class="btn btn-success done") when clicked disables the card and hides the button.
     document.getElementById("box").innerHTML += `
+    <div>
     <div class="card m-3 everyCard" id="cardId" style="width: 21rem; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
         <img src="/img/blank-papwer-pen.jpg" class="card-img-top" style="height: 40%; width: 100%" alt="${val.title}">
         <div class="card-body">
@@ -18,12 +23,12 @@ for (let val of allTasks) {
             <p class="card-text text-center">${val.description}</p>
             <hr>
             <div class="d-flex">
-                <p class="card-text" style="margin: 0">&#9888; Priority Level</p>
-                <button type="button" class="btnPriority text-white" style="background-color: ${color}; border: none; border-radius: 5px; height: 25px; margin-left: 15px">${val.importance}</button>
+                <p class="card-text">&#9888; Priority Level</p>
+                <button type="button" class="btnPriority text-white" style="background-color: ${color}; border: none; border-radius: 5px; height: 25px; width: 25px; margin-left: 15px">${val.importance}</button>
             </div>
             <hr>
             <div>
-                <p class="card-text" style="margin: 0">Duration: ${val.duration}</p>
+                <p class="card-text"">Duration: ${val.duration}</p>
             </div>
             <hr>
             <div class="text-end p-1" style="position: relative; bottom: 5px; right: 1px">
@@ -32,9 +37,11 @@ for (let val of allTasks) {
             </div>
         </div>
     </div>
+    </div>
     `;
 }
 
+//add event to the priority button - click adds number up to 5 and goes back to 0, depending on number, button color changes
 let btnsPrior = document.getElementsByClassName("btnPriority");
 
 for (let i = 0; i < btnsPrior.length; i++) {
@@ -57,19 +64,20 @@ for (let i = 0; i < btnsPrior.length; i++) {
 
 }
 
+//add event to the delete button - click removes card from the page
 function btnsDelete() {
     let btnDel = document.getElementsByClassName("delete");
     let cardArr = document.getElementsByClassName("everyCard");
 
     for (let i = 0; i < btnDel.length; i++) {
         btnDel[i].addEventListener("click", function() {
-            cardArr[i].remove();
+            cardArr[i].innerHTML = "";
         })
     }
 }
 btnsDelete();
 
-
+//add event to the done button - click hides done button and turns card grey, so we don't confuse it with active cards
 function btnsDone() {
     let btnDone = document.getElementsByClassName("done");
     let cardArr = document.getElementsByClassName("everyCard");
@@ -78,7 +86,7 @@ function btnsDone() {
         btnDone[i].addEventListener("click", function() {
 
             cardArr[i].style.filter = "grayscale(100%)";
-            btnDone[i].remove();
+            btnDone[i].style.display = "none";
         })
     }
 }
